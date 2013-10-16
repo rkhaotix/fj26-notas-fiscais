66ce474f5e85392ae7576d2250fd22bd229fa668
Raphael A. Silva <rkhaotix@gmail.com>
2013-10-16 09:11:58 -0300

    Melhoria na autenticação

--- src/br/com/caelum/notasfiscais/mb/LoginBean.java
+++ src/br/com/caelum/notasfiscais/mb/LoginBean.java
@@ -1,23 +1,38 @@
 package br.com.caelum.notasfiscais.mb;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 import br.com.caelum.notasfiscais.dao.UsuarioDao;
 import br.com.caelum.notasfiscais.modelo.Usuario;
 
-@SessionScoped
-@ManagedBean
-public class LoginBean {
+@Named
+@RequestScoped
+public class LoginBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private Usuario usuario = new Usuario();
 	
+	@Inject
+	private UsuarioDao dao;
+	
+	@Inject
+	private UsuarioLogadoBean usuarioLogado;
+	
 	public String efetuaLogin() {
-		UsuarioDao dao = new UsuarioDao();
 		boolean loginValido = dao.existe(this.usuario);
 		if(loginValido) {
+			usuarioLogado.logar(usuario);
 			return "produto?faces-redirect=true";
 		}
 		else {
+			usuarioLogado.deslogar();
 			this.usuario = new Usuario();
 			return "login";
 		}
